a7e51b06ed5e38f07666b7b4bc03f8b7ecf72320
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2025-02-07 22:14:57 +0100

    Remove Prettier integration from ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    rules: {
-      'prettier/prettier': 'error', // 🔥 Integración de Prettier con ESLint
-    },
-  },
 ]
 
 export default eslintConfig
