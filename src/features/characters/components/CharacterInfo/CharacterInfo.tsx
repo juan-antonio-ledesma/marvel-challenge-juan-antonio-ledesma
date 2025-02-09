febb156c550d5aabde52c5e66b545524a8d3ed3c
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2025-02-09 20:05:26 +0100

    Refactor CharacterInfo styles for improved layout and responsiveness; update image component for better styling

--- src/features/characters/components/CharacterInfo/CharacterInfo.tsx
+++ src/features/characters/components/CharacterInfo/CharacterInfo.tsx
@@ -20,7 +20,13 @@ export default function CharacterInfo({
   return (
     <section className={styles.root}>
       <div className={styles.innerWrapper}>
-        <Image src={image} alt={name} width={320} height={320} />
+        <Image
+          className={styles.image}
+          src={image}
+          alt={name}
+          width={320}
+          height={320}
+        />
         <div className={styles.mainContent}>
           <div className={styles.header}>
             <h1 className={styles.title}>{name}</h1>
