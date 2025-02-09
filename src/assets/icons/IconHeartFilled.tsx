10da54a731ad1e71898a1fb5713f6375705d7acf
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2025-02-09 11:57:10 +0100

    Add aria-hidden attribute to icon components for improved accessibility

--- src/assets/icons/IconHeartFilled.tsx
+++ src/assets/icons/IconHeartFilled.tsx
@@ -6,6 +6,7 @@ export default function IconHeartFilled() {
       viewBox="0 0 13 12"
       fill="currentColor"
       className="icon"
+      aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path d="M6.71436 2.37318L3.71435 0.552368L0.714355 2.37318V6.27491L6.71436 11.3905L12.7144 6.27491V2.37318L9.71436 0.552368L6.71436 2.37318Z" />
